ad6dab8d287380aa50ed309f526f9d9e762f29c4
Luke <ReventonC@users.noreply.github.com>
2018-06-22 01:16:11 -0700

    Init (Header on going)

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+      <app-header></app-header> 
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  components: {
+      'app-header': Header
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <img id="logo" src="../assets/JSAlogoattemptred.png" alt="UCSD_JSA_LOGO">
+    </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped lang="scss">
+    $logo-w-and-h: 100px;
+    #logo {
+        width: $logo-w-and-h;
+        height: $logo-w-and-h;
+    }
+</style>
